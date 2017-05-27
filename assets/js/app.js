e3a63fb70ff1eea9af20479c22af0e3459255628
Jose Daniel Posso Garcia <daniel7byte@gmail.com>
2017-05-26 19:37:01 -0500

    First Commit
    Falta el Maping para el records.html

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+(function() {
+
+  // Initialize Firebase
+  const config = {
+    apiKey: "AIzaSyDUkEM3HQoTCGT9NKul0vOI88ZcW3W4bmU",
+    authDomain: "my-location-records-f2a02.firebaseapp.com",
+    databaseURL: "https://my-location-records-f2a02.firebaseio.com",
+    projectId: "my-location-records-f2a02",
+    storageBucket: "my-location-records-f2a02.appspot.com",
+    messagingSenderId: "1035519205891"
+  };
+  firebase.initializeApp(config);
+
+} ());
